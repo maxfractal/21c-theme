187ce37283d0eafbd39cd9b69fe7c408b2cc7618
Xris <cp@maxfractal.com>
2022-01-05 22:53:25 -0500

    First function loop

--- 21cgeek/wp-content/themes/21cgeek/index.php
+++ 21cgeek/wp-content/themes/21cgeek/index.php
@@ -6,3 +6,11 @@ Starter file for 21cgeek theme.
 
 ?>
 </p>
+<?php 
+    while(have_posts()) {
+        the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        <hr>
+    <?php }
+?>
