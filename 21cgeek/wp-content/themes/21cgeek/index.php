6b48312b56b5141cea283e02b8072284c7e5a317
Xris <cp@maxfractal.com>
2022-01-05 23:10:19 -0500

    Starting to build individual blog posts.

--- 21cgeek/wp-content/themes/21cgeek/index.php
+++ 21cgeek/wp-content/themes/21cgeek/index.php
@@ -9,7 +9,7 @@ Starter file for 21cgeek theme.
 <?php 
     while(have_posts()) {
         the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php the_content(); ?>
         <hr>
     <?php }
