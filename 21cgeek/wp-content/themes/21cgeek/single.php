63a6776ad2f9003863fadb97fb21d0d29d75ddd4
Xris <cp@maxfractal.com>
2022-01-10 10:17:00 -0500

    Testing some CSS with theme

--- 21cgeek/wp-content/themes/21cgeek/single.php
+++ 21cgeek/wp-content/themes/21cgeek/single.php
@@ -2,9 +2,13 @@
 
 get_header();
 
+?>
+This is single.php.
+<div class="box">This is a box.</div>
+<?php
     while(have_posts()) {
         the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+        <div class="box"><h2><?php the_title(); ?></h2></div>
         <?php the_content(); ?>
     <?php }
 
