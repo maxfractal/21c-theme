2603d92b9df922ff134c7178d14e2d28a7e81666
Xris <cp@maxfractal.com>
2023-03-03 18:13:45 -0500

    Missed updates for blog posts

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>..::Product UX Otaku::..</title>
+</head>
+<body>
+    <?php
+    define( 'WP_USE_THEMES', true );
+    require __DIR__ . '/21cgeek/wp-blog-header.php';
+ 
+    ?>
+</body>
+</html>
